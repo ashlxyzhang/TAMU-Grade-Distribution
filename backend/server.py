3d88f634a0649ea719c2e2f27c1a364c1067bdb2
Ashley <ashleyoftexas@gmail.com>
2023-11-16 11:11:12 -0600

    display data from mongodb and search func

--- backend/server.py
+++ backend/server.py
@@ -1,12 +1,29 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, request
 from flask_cors import CORS
-import pandas as pd
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from bson import json_util
+import os
 
 app = Flask(__name__)
 CORS(app)
 
-df = pd.read_csv('backend/grds/arch.csv')
-json = df.to_json()
+uri = f"mongodb+srv://{os.environ.get('MONGO_USER')}:{os.environ.get('MONGO_PASS')}@tamugrds.nnsegmo.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+db = client['TAMUgrds']
+collection = db['collection']
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+# df = pd.read_csv('backend/grds/arch.csv')
+# json = df.to_json()
 
 # @app.route('/')
 # def html_table():
@@ -14,7 +31,17 @@ json = df.to_json()
 
 @app.route('/data')
 def get_grds():
-    return json
+    dep = request.args.get('dep')
+    course = request.args.get('course')
+    prof = request.args.get('prof')
+
+    print(dep, course, prof)
+
+    query = {}
+    if dep and course: query['COURSE'] = dep + " " + course
+    if prof: query['PROF'] = { '$regex': prof }
+
+    return json_util.dumps(list(collection.find(query))) if query != {} else []
 
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend/src/components/FetchGRDs.tsx
+++ frontend/src/components/FetchGRDs.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Form from "./Form";
 
 interface Props {
   dep: string;
@@ -8,87 +7,110 @@ interface Props {
 }
 
 const FetchGRDs = ({ dep, course, prof }: Props) => {
-  const [data, setData] = useState({
-    COURSE: [""],
-    PROF: [""],
-    GPA: [0],
-    A: [0],
-    B: [0],
-    C: [0],
-    D: [0],
-    F: [0],
-    "A (%)": [0],
-    "B (%)": [0],
-    "C (%)": [0],
-    "D (%)": [0],
-    "F (%)": [0],
-    Q: [0],
-  });
+  const [data, setData] = useState([]);
+  const [headers, setHeaders] = useState([""]);
+  //   COURSE: [""],
+  //   PROF: [""],
+  //   GPA: [0],
+  //   A: [0],
+  //   B: [0],
+  //   C: [0],
+  //   D: [0],
+  //   F: [0],
+  //   "A (%)": [0],
+  //   "B (%)": [0],
+  //   "C (%)": [0],
+  //   "D (%)": [0],
+  //   "F (%)": [0],
+  //   Q: [0],
+  // ]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await fetch("http://127.0.0.1:5000/data");
+        const res = await fetch(
+          `http://127.0.0.1:5000/data?dep=${dep}&course=${course}&prof=${prof}`
+        );
         if (!res.ok) {
           throw new Error("Data not fetched");
         }
         const result = await res.json();
+        setData(result);
+        setHeaders(Object.keys(result[0]));
 
-        Object.entries(data).forEach(([key, values]) => {
-          if (result.hasOwnProperty(key)) {
-            setData((prevData) => ({
-              ...prevData,
-              [key]: Object.values(result[key]),
-            }));
-          }
-        });
+        console.log(result);
+
+        // Object.entries(data).forEach(([key, values]) => {
+        //   if (result.hasOwnProperty(key)) {
+        //     setData((prevData) => ({
+        //       ...prevData,
+        //       [key]: Object.values(result[key]),
+        //     }));
+        //   }
+        // });
       } catch (error) {
         console.error(error);
       }
     };
 
     fetchData();
-  }, []);
+  }, [dep, course, prof]);
 
-  const displayData = (row: number, col: number, key: string) => {
-    if (
-      dep !== "" &&
-      course !== "" &&
-      data.COURSE[row] === dep + " " + course
-    ) {
-      if (data.PROF[row].includes(prof) || prof === "") {
-        return (
-          <td key={col}>
-            {(data as Record<string, number[] | string[]>)[key][row]}
-          </td>
-        );
-      }
-    } else if (data.PROF[row].includes(prof) || prof === "") {
-      return (
-        <td key={col}>
-          {(data as Record<string, number[] | string[]>)[key][row]}
-        </td>
-      );
-    }
-  };
+  // const displayData = (row: number, col: number, key: string) => {
+  //   if (dep === "" && course === "" && prof === "") {
+  //     return "";
+  //   }
+  //   if (
+  //     dep !== "" &&
+  //     course !== "" &&
+  //     data['COURSE'] === dep + " " + course
+  //   ) {
+  //     if (data.PROF[row].includes(prof) || prof === "") {
+  //       return (
+  //         <td key={col}>
+  //           {(data as Record<string, number[] | string[]>)[key][row]}
+  //         </td>
+  //       );
+  //     }
+  //   } else if (dep === "" && data.PROF[row].includes(prof)) {
+  //     return (
+  //       <td key={col}>
+  //         {(data as Record<string, number[] | string[]>)[key][row]}
+  //       </td>
+  //     );
+  //   }
+  // };
 
-  return (
-    <>
-      <div className="container d-flex justify-content-center">
-        <table className="table table-striped table-bordered">
-          <thead>
-            <tr style={{ backgroundColor: "lightblue" }}>
-              {Object.entries(data).map(([key, values], index) => (
+  if (data.length !== 0) {
+    return (
+      <>
+        <div className="container d-flex justify-content-center">
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr style={{ backgroundColor: "lightblue" }}>
+                {headers.map(
+                  (header, index) =>
+                    header !== "_id" && <th key={index}>{header}</th>
+                )}
+                {/* {Object.entries(data).map(([key, values], index) => (
                 <th scope="col" key={index}>
                   {key}
                 </th>
+              ))} */}
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((record, row) => (
+                <tr key={row}>
+                  {headers.map(
+                    (header, col) =>
+                      header !== "_id" && <td key={col}>{record[header]}</td>
+                  )}
+                </tr>
               ))}
-            </tr>
-          </thead>
-          <tbody>
-            {/* data.A[0], data.B[0], etc. */}
-            {/* data.A[1], data.B[1], etc. */}
-            {(data as Record<string, number[] | string[]>)[
+              {/* data.A[0], data.B[0], etc. */}
+              {/* data.A[1], data.B[1], etc. */}
+              {/* {(data as Record<string, number[] | string[]>)[
               Object.keys(data)[0]
             ].map((_, row) => (
               <tr key={row}>
@@ -96,12 +118,17 @@ const FetchGRDs = ({ dep, course, prof }: Props) => {
                   displayData(row, col, key)
                 )}
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </>
-  );
+            ))} */}
+            </tbody>
+          </table>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <div style={{ fontSize: 20 }}>No data found. Check your inputs?</div>
+    );
+  }
 };
 
 export default FetchGRDs;
